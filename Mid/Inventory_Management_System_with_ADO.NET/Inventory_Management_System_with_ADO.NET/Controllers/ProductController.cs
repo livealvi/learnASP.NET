113c2be97526b039a5e9a2ba7632fe76fc45e6c3
livealvi <livealvi@gmail.com>
2022-05-17 00:50:20 +0600

    view: Product list

---
+++ Mid/Inventory_Management_System_with_ADO.NET/Inventory_Management_System_with_ADO.NET/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using Inventory_Management_System_with_ADO.NET.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Inventory_Management_System_with_ADO.NET.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            ProductDataAccess pda = new ProductDataAccess();
+            return View(pda.GetAllProducts());
+        }
+    }
+}
---
+++ Mid/Inventory_Management_System_with_ADO.NET/Inventory_Management_System_with_ADO.NET/Models/ProductDataAccess.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Inventory_Management_System_with_ADO.NET.Models
+{
+    public class ProductDataAccess
+    {
+        DataAccess dataAccess;
+
+        public ProductDataAccess()
+        {
+            this.dataAccess = new DataAccess();
+        }
+
+        public List<Product> GetAllProducts()
+        {
+            string sql = "SELECT * FROM Products";
+            SqlDataReader reader = dataAccess.GetData(sql);
+            List<Product> productList = new List<Product>();
+            while (reader.Read())
+            {
+                Product pro = new Product();
+                pro.ProductId = (int)reader["ProductId"];
+                pro.ProductName = (string)reader["ProductName"].ToString();
+                pro.Price = Convert.ToDouble(reader["Price"]);
+                pro.CategoryId = (int)reader["CategoryId"];
+                productList.Add(pro);
+            }
+            return productList;
+        }
+
+
+    }
+}
